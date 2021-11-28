7e38b37e15d8fc0eba3915a06f7d71fc3fade954
Jean Khoury <jeankhoury0@gmail.com>
2021-11-28 00:16:06 -0500

    Fixed accessibility issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,35 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  mode: "jit",
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-      extend: {
+    extend: {
       colors: {
-          mainAccent: '#406576',
-          secondAccent: '#3AAFA9',
-          secondAccAccent:'#349d98',
-          grayAccent: '#17242A',
-          creme: '#D8C3A4',
-          whiteoff: '#EAE8DC',
-          linkedin: '#0073b1',
+        mainAccent: "#406576",
+        secondAccent: "#268187",
+        secondAccAccent: "#349d98",
+        grayAccent: "#17242A",
+        creme: "#D8C3A4",
+        whiteoff: "#EAE8DC",
+        linkedin: "#0073b1",
       },
       minHeight: {
-        '2.5': '2.5em',
-        '3.5': '3.5em',
-        '4.5': '4.5em',
-        '5.5': '5.5em',
-
-      }
+        2.5: "2.5em",
+        3.5: "3.5em",
+        4.5: "4.5em",
+        5.5: "5.5em",
+      },
     },
   },
   variants: {
     extend: {
       fontFamily: {
-        'mont': ['Montserrat', 'Helvetica', 'Arial', 'sans-serif']
-      }
+        mont: ["Montserrat", "Helvetica", "Arial", "sans-serif"],
+      },
     },
   },
   plugins: [
-    require('@tailwindcss/typography'),
+    require("@tailwindcss/typography"),
     // require('@tailwindcss/forms'),
   ],
-}
+};
